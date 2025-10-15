476b13b267d743c7f3b54b9185b4663fc890cc7d
mbahjadol <mbah.jadol@gmail.com>
2025-10-15 22:50:18 +0700

    update for calling flink-sql-client

--- flink-sql-client.sh
+++ flink-sql-client.sh
@@ -4,4 +4,7 @@ if [ "$(docker inspect -f '{{.State.Running}}' flink-jobmanager 2>/dev/null)" !=
   echo "❌ Flink JobManager is not running. Please start the environment first by running ./init.sh"
   exit 1
 fi
-docker exec -it flink-jobmanager /opt/flink/bin/sql-client.sh
+docker exec -it flink-jobmanager /opt/flink/bin/sql-client.sh \
+    -j /opt/flink/usrlib/flink-sql-connector-mysql-cdc-3.5.0.jar \
+    -j /opt/flink/usrlib/flink-connector-jdbc-3.3.0-1.20.jar \
+    -j /opt/flink/usrlib/mysql-connector-j-8.3.0.jar
