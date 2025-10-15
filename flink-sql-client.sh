ae8df4613ecf04147b8271077b1869dd53b345fd
mbahjadol <mbah.jadol@gmail.com>
2025-10-15 10:54:13 +0700

    update flink-sql-client

--- flink-sql-client.sh
+++ flink-sql-client.sh
@@ -5,36 +5,3 @@ if [ "$(docker inspect -f '{{.State.Running}}' flink-jobmanager 2>/dev/null)" !=
   exit 1
 fi
 docker exec -it flink-jobmanager /opt/flink/bin/sql-client.sh
-
-
-# CREATE TABLE mysql_source_users (
-#   id INT,
-#   name STRING,
-#   ts TIMESTAMP(3),
-#   PRIMARY KEY (id) NOT ENFORCED
-# ) WITH (
-#   'connector' = 'mysql-cdc',
-#   'hostname' = 'mysql-source',
-#   'port' = '3306',
-#   'username' = 'repl',
-#   'password' = 'replpwd',
-#   'database-name' = 'testdb',
-#   'table-name' = 'users',
-#   'scan.startup.mode' = 'initial'
-# );
-
-# CREATE TABLE mysql_target_users (
-#   id INT,
-#   name STRING,
-#   ts TIMESTAMP(3),
-#   PRIMARY KEY (id) NOT ENFORCED
-# ) WITH (
-#   'connector' = 'jdbc',
-#   'url' = 'jdbc:mysql://mysql-target:3306/testdb',
-#   'table-name' = 'users',
-#   'username' = 'root',
-#   'password' = 'rootpwd'
-# );
-
-# INSERT INTO mysql_target_users
-# SELECT * FROM mysql_source_users;
